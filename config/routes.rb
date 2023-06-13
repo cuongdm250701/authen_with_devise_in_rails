74899e457a6cc53fe3f865cd9dd48eab43960b91
cuongdm250701 <domanhcuong250701@gmail.com>
2023-06-13 09:34:45 -0700

    init authen with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get "home" => 'home#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  as :user do
+    # Auth
+    get "signin" => "users/sessions#new"
+    post "signin" => "users/sessions#create"
+    delete "signout" => "users/sessions#destroy"
+    # Register
+    get "signup" => "users/registrations#new"
+    post "signup" => "users/registrations#create"
+    get "signup" => "users/registrations#edit"
+    put "signup" => "users/registrations#update"
+    delete "signup" => "users/registrations#destroy"
+  end
 end
